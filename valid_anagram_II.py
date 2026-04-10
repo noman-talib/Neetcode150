b91c8f1a2ebb329a2588d8b28c6f33fc36c4ea0a
noman-talib <nomantalibnoman@gmail.com>
2026-04-10 11:48:41 +0500

    This is leetcode 242

---
+++ valid_anagram_II.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        sorted_one = sorted(s)
+        sorted_two = sorted(t)
+        if sorted_one == sorted_two:
+            return True
+        else:
+            return False
